660eb54112f092d19221dd8b968aab485326ab63
SandaruKasa <sandarukasa@ya.ru>
2021-09-14 13:21:01 +0300

    Fancy board output

--- src/draft.py
+++ src/draft.py
@@ -3,25 +3,26 @@
 # board[y][x]
 # board[row - 1][column - 1]
 
-def pre_main(itertools):
+def pre_main(itertools, math):
     def main(players, ask_user):
         def pre_game(m, n, k):
-            def game(board):
+            def game(board, format_board):
                 def make_move(player):
                     def process_move(y, x):
                         def check_victory(direction):
                             def count(dx, dy):
                                 return len([*itertools.takewhile(lambda delta: (
-                                        0 <= x + dx * delta < m and
-                                        0 <= y + dy * delta < n and
+                                        0 <= x + dx * delta < n and
+                                        0 <= y + dy * delta < m and
                                         board[y + dy * delta][x + dx * delta] == player
                                 ), itertools.count(1))])
 
                             return count(*direction) + count(-direction[0], -direction[1]) >= k - 1
 
                         return (board[y].__setitem__(x, player) or
-                                all(all(map('.'.__ne__, row)) for row in board) and "Draw!" or
-                                any(map(check_victory, [(0, 1), (1, 0), (1, 1), (1, -1)])) and f"Player {player} won!")
+                                all(all(map('.'.__ne__, row)) for row in board) and f"{format_board(board)}\n\nDraw!" or
+                                any(map(check_victory, [(0, 1), (1, 0), (1, 1), (1, -1)]))
+                                and f"{format_board(board)}\n\nPlayer {player} won!")
 
                     def validate_move(row, column):
                         return (column < 1 and "Column should be >= 1" or
@@ -30,10 +31,7 @@ def pre_main(itertools):
                                 row > m and f"Row should be <= {m}" or
                                 board[row - 1][column - 1] != '.' and "This cell is already occupied")
 
-                    def format_board():
-                        return "\n".join(''.join(row) for row in board)  # todo: row and column numbers
-
-                    return print(format_board()) or process_move(*(
+                    return print(format_board(board)) or process_move(*(
                         c - 1 for c in ask_user(f"Player {player}, please, enter row and column", validate_move)
                     ))
                     # end make_move
@@ -41,7 +39,13 @@ def pre_main(itertools):
                 return next(itertools.dropwhile(lambda a: not a, map(make_move, players)))
                 # end game
 
-            return game([['.'] * n for _ in range(m)])
+            def format_board_impl(board):
+                format_str = "{:>" + str(math.floor(math.log(max(m, n), 10) + 1)) + "}"
+                board_to_format = [[*range(1, n + 1)]] + board
+                return "\n".join(
+                    ' '.join(map(format_str.format, [row_no] + board_to_format[row_no])) for row_no in range(m + 1))
+
+            return game([['.'] * n for _ in range(m)], format_board_impl)
             # end pre_game
 
         def validate_mnk(m, n, k):
@@ -65,4 +69,4 @@ def pre_main(itertools):
 
 
 if __name__ == '__main__':
-    print(pre_main(__import__("itertools")))
+    print(pre_main(__import__("itertools"), __import__("math")))
